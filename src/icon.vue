98b8ab0078039943041e9d88d348567b656e932f
Igor <30553932+Yanchevskiy@users.noreply.github.com>
2018-07-21 11:56:16 +0800

    Update icon.vue

--- src/icon.vue
+++ src/icon.vue
@@ -24,7 +24,7 @@ export default {
   },
   computed: {
     svgFullDir: function () {
-      return "assets/sprite.svg" + this.element.icon
+      return "src/assets/sprite.svg" + this.element.icon
     } 
   }
 }
